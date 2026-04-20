5290c3cf57e1d4710060ae34fd4d32841a71778f
Administrator <Administrator@PMT2023>
2026-04-20 15:58:16 +0700

    buoi4.1

--- KyThuatLapTrinh/buoi4/buoi4.cpp
+++ KyThuatLapTrinh/buoi4/buoi4.cpp
@@ -1,84 +1,76 @@
 #include <iostream>
 #include <string>
-#include <Account.h>
-#include <LinkedList.h>
+#include "Account.h"
+#include "LinkedList.h"
+
 using namespace std;
 
 int main()
 {
-    LinkedList<Account> account = { NULL };
+    LinkedList <Account> accounts = { NULL };
     do {
         system("cls");
-        cout << "________BOOK MANAGEMENT_______" << endl;
-        cout << "1.Show all books" << endl;
-        cout << "2.Add a book" << endl;
-        cout << "3.Delete a book" << endl;
-        cout << "4.Update a book" << endl;
-        cout << "5.Find book" << endl;
-        cout << "6.Export to file" << endl;
-        cout << "7.Import from file" << endl;
-        cout << "0.Exit" << endl;
-        cout << "________________________________" << endl;
+        cout << "----- Account MANAGEMENT ------------" << endl;
+        cout << "1. Show all account" << endl;
+        cout << "2. Add an account" << endl;
+        cout << "3. Delete an account" << endl;
+        cout << "4. Update an account" << endl;
+        cout << "5. Find account" << endl;
+        cout << "6. Export to file" << endl;
+        cout << "7. Import from file" << endl;
+        cout << "0. Exit" << endl;
+        cout << "--------------------------" << endl;
         cout << "Enter your choice: ";
         int choice;
         cin >> choice;
-        switch (choice) {
+        switch (choice)
+        {
         case 1: {
-            //books.Show();
+            accounts.Show();
             break;
         }
         case 2: {
-            /*Book b;
-            cin >> b;
-            Node* newNode = new Node;
-            newNode->Create(b);
-            books.AddFirst(newNode);*/
+            Account a;
+            cin >> a;
+            accounts.Add(a);
             break;
         }
         case 3: {
             int removeId;
-            cout << "Enter book's id to remove: ";
+            cout << "Enter account id to remove: ";
             cin >> removeId;
-            bool res = books.Remove(removeId);
+            bool res = accounts.Remove(removeId);
             if (res)
-                cout << "Remove book successfully" << endl;
+                cout << "Remove account with id: " << removeId << endl;
             else
-                cout << "Invalid vook id" << endl;
+                cout << "Not found account id: " << removeId << endl;
             break;
         }
         case 4: {
             int updateId;
-            cout << "Enter book's id to update: ";
+            cout << "Enter account id to update: ";
             cin >> updateId;
-            bool res = books.Update(updateId);
+            bool res = accounts.Update(updateId);
             if (res)
-                cout << "Update book successfully" << endl;
+                cout << "Update account successfully" << endl;
             else
-                cout << "Invalid book id" << endl;
+                cout << "Invalid account id" << endl;
             break;
         }
         case 5: {
-            string bookName;
-            cout << "Enter book ' s name to find : ";
+            string userName;
+            cout << "Enter account name: ";
             cin.ignore();
-            getline(cin, bookName);
-            Book* res = books.Find(bookName);
-            if (res != NULL) {
-                cout << *res;
-            }
-            else {
-                cout << "No book with name : " << bookName << endl;
-            }
+            getline(cin, userName);
+            accounts.Find(userName);
             break;
         }
         case 6: {
-           // books.Export("25TH1.dla");
-            //cout << "Exported successfully" << endl;
+
             break;
         }
         case 7: {
-            //books.Export("25TH1.dla");
-            //cout << "Exported successfully" << endl;
+
             break;
         }
         case 0: {
@@ -90,6 +82,7 @@ int main()
         }
         }
         system("pause");
-        cout << "Press any key to continue...";
+        cout << "Press any key to continue....";
+
     } while (true);
 }
